8e746cef403ae5e19c94b60464b7de2229489e14
tikisailor <40813246+tikisailor@users.noreply.github.com>
2021-09-21 22:30:47 +0800

    set up repo

--- test.js
+++ test.js
@@ -15,17 +15,6 @@ const query = gql`
 }
 `
 
-// request('https://api.subquery.network/sq/tikisailor/polkawatch__dGlra', query).then((data) => {
-//   // console.log(data)
-//   // console.log(data.validatorThresholds.nodes[1].startBlock);
-//   const ob = data.validatorThresholds.nodes.find(obj => obj.startBlock === 4456306);
-//   console.log(Object.keys(ob).length);
-//   console.log(ob.validatorList.length);
-//   console.log(data.validatorThresholds.nodes.find(obj => obj.startBlock === 4456306));
-//   // console.log(data.validatorThresholds.nodes[0].validatorList);
-// });
-
-
 describe("API is responding",function(){
     it("Check if query returns expected result",function(){
       request('https://api.subquery.network/sq/tikisailor/polkawatch__dGlra', query).then((data) => {
